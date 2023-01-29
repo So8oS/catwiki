35b0a1718981dadced8b0e46d56e372d051f5b01
So8oS <ahmadothman033@gmail.com>
2023-01-29 18:14:18 +0300

    md mx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import Intro from "../compnents/Intro";
 const Home: NextPage = () => {
   return (
     <div className="flex flex-col justify-center items-center px-10 py-3 
-    
+    md:mx-12 
     lg:mx-18 
     xl:mx-26 
     2xl:mx-56
