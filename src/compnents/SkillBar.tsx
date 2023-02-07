03098dc489e41645ba047fc0a159f155ce426c8a
Ahmad Othman <62267611+So8oS@users.noreply.github.com>
2023-02-08 01:58:35 +0300

    fix

--- src/compnents/SkillBar.tsx
+++ src/compnents/SkillBar.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import React from "react";
-const SkillBar = ({ level }) => {
+// @ts-ignore
+const SkillBar = ({ level}) => {
   // create a variable to store the temperament levels
   const Levels = [1, 2, 3, 4, 5];
   // create a variable to store the temperament colors
